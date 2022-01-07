a89cda146c0c4237e5a27ad0af0604b16483653a
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2022-01-07 14:54:40 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,44 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 1/q_1.py
+Please give number 1: 5
+Please give number 2: 6
+Please give number 3: 7
+6.0
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 1/q_2.py
+Please enter gross income: 100000
+Enter the no. of dependents: 3
+taxable income:
+81000.0
+tax:
+16200.0
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 1/q_3.py
+Please enter name: Pratham Bansal
+Enter student Id: 21103105
+Enter gender: M
+Enter your course name: CSE
+Enter cgpa: 10.0
+['Name', 'SID', 'Gender', 'Course name', 'Cgpa']
+['Pratham Bansal', 21103105, 'M', 'CSE', 10.0]
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 1/q_4.py
+Enter student 1 marks: 94
+Enter student 2 marks: 92
+Enter student 3 marks: 95
+Enter student 4 marks: 96
+Enter student 5 marks: 97
+List: 
+[94, 92, 95, 96, 97]
+Sorted List(decreasing order): 
+[97, 96, 95, 94, 92]
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 1/q_5.py
+(a)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+(b)
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+['Red', 'Green', 'White', 'Purple', 'Yellow']
