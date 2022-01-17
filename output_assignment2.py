bc46190d2c98bc046e0631ff94d5ec35b229d59e
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2022-01-17 15:48:55 +0530

    Add files via upload

---
+++ output_assignment2.py
@@ -0,0 +1,53 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_1.py
+(a)
+Give the statement:Python is a case sensitive language
+Python is a case sensitive language
+35
+(b)
+egaugnal evitisnes esac a si nohtyP
+(c)
+a case sensitive
+(d)
+Python is object oriented language
+(e)
+10
+(f)
+Pythonisacasesensitivelanguage
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_2.py
+Hey, Pratham Bansal Here!
+My SID is 21103105 
+I am from CSE department and my CGPA is 9.9
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_3.py
+(a)
+0b1000
+(b)
+0b111010
+(c)
+0b110010
+(d)
+0b11100000
+0b101000
+(e)
+0b1110
+0b0
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_4.py
+Give a number:77
+Give a number:88
+Give a number:99
+99 is greatest number
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_5.py
+Give a word:namesake
+Yes
+
+= RESTART: /Users/prathambansal/Downloads/cse/INTRO TO COMPUTING/assignment 2/a_2q_6.py
+Give a number:4
+Give a number:5
+Give a number:6
+The triangle is possible
