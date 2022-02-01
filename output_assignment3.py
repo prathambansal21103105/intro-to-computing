cbdb7b577af240ffc59299d3a80fe96e683975ce
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2022-02-01 14:19:59 +0530

    Add files via upload

---
+++ output_assignment3.py
@@ -0,0 +1,123 @@
+Python 3.10.1 (v3.10.1:2cd268a3a9, Dec  6 2021, 14:28:59) [Clang 13.0.0 (clang-1300.0.29.3)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_1.py =
+Give a string-arshggjgsra
+{'a': 2, 'r': 2, 's': 2, 'h': 1, 'g': 3, 'j': 1}
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_2.py =
+Give month-12
+Give day-31
+Give year-1800
+Date- 31 / 12 / 1800
+Next Date- 1/1/ 1801
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_3.py =
+[(1, 1), (2, 4), (3, 9), (4, 16), (5, 25), (6, 36), (7, 49), (8, 64), (9, 81), (10, 100)]
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_4.py =
+Give a number between 4 and 10 including them-9
+Performance=Excellent
+Letter Grade=A
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_5.py =
+ ABCDEFGHIJK
+  ABCDEFGHI
+   ABCDEFG
+    ABCDE
+     ABC
+      A
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_6.py =
+----------------(a)-------------------
+Give the sid (a number):9
+Enter your name:l
+give a letter y or n:y
+Give the sid (a number):8
+Enter your name:z
+give a letter y or n:y
+Give the sid (a number):1
+Enter your name:j
+give a letter y or n:y
+Give the sid (a number):3
+Enter your name:y
+give a letter y or n:n
+The required dictionary-- {9: 'l', 8: 'z', 1: 'j', 3: 'y'}
+The list will be used in further parts--
+The required list-- [(9, 'l'), (8, 'z'), (1, 'j'), (3, 'y')]
+Done!
+----------------(b)------------------------
+The dictionary we had-
+{9: 'l', 8: 'z', 1: 'j', 3: 'y'}
+now we have exchanged the key value pair but its not sorted-
+{'l': 9, 'z': 8, 'j': 1, 'y': 3}
+The unsorted list--
+[('l', 9), ('z', 8), ('j', 1), ('y', 3)]
+The sorted list--
+[('j', 1), ('l', 9), ('y', 3), ('z', 8)]
+The sorted dictionary-
+{'j': 1, 'l': 9, 'y': 3, 'z': 8}
+This is the dictionary sorted on the basis of name-
+{1: 'j', 9: 'l', 3: 'y', 8: 'z'}
+Done!
+-----------------(c)-----------------
+The list we made before will be used now-
+[(9, 'l'), (8, 'z'), (1, 'j'), (3, 'y')]
+now the list is sorted-
+[(1, 'j'), (3, 'y'), (8, 'z'), (9, 'l')]
+sorted dictionary based on sid- {1: 'j', 3: 'y', 8: 'z', 9: 'l'}
+Done!
+-----------------(d)-----------------
+Please enter one of the sids you entered before-1
+The name of the student with the entered sid is-
+j
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_7.py =
+Give a number-1
+Give a number-2
+[1, 2, 3]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+Give y to contnue and n to stop going further-y
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Give y to contnue and n to stop going further-n
+Now we got a list having a fibonacci series-
+[1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+Average of the sequence-
+34.09090909090909
+Done!
+
+== RESTART: /Users/prathambansal/Downloads/cse python/assignment python/q_8.py =
+-------------(a)------------
+{1, 3, 5, 6, 8}
+Done!
+-------------(b)------------
+{3, 6, 8, 9, 13, 17}
+Done!
+-------------(c)------------
+{1, 2, 4, 5}
+Done!
+-------------(d)------------
+{6, 7, 8, 9, 10}
+Done!
+-------------(e)------------
+{10, 7}
+Done!
