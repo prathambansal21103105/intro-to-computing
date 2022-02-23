09f602240737eeba0f6949c0f79cc54dc3566d5a
Pratham Bansal <bansalpratham.bt21cse@pec.edu.in>
2022-02-23 17:33:04 +0530

    Add files via upload

---
+++ output_assignment4.py
@@ -0,0 +1,81 @@
+/usr/local/bin/python3 "/Users/prathambansal/Downloads/python/cse python/assignment 4/assignment_4.p%                                                 y"                                                                                                  
+prathambansal@192 python % /usr/local/bin/python3 "/Users/prathambansal/Downloads/python/cse python/assignment 4/assignment_4.py"
+-------------1--------------
+Transfer disk from A to C
+Transfer disk from A to B
+Transfer disk from C to B
+Transfer disk from A to C
+Transfer disk from B to A
+Transfer disk from B to C
+Transfer disk from A to C
+-------------2(A)------------
+Using iteration-
+Give the no. of rows-7
+      1  
+
+     1  1  
+
+    1  2  1  
+
+   1  3  3  1  
+
+  1  4  6  4  1  
+
+ 1  5  10  10  5  1  
+
+1  6  15  20  15  6  1  
+
+--------------2(B)-------------
+Using recursion-
+Please give no. of rows-7
+              1 
+
+             1 1 
+
+            1 2 1 
+
+           1 3 3 1 
+
+          1 4 6 4 1 
+
+         1 5 10 10 5 1 
+
+        1 6 15 20 15 6 1 
+
+Done!
+-------------3(A)-----------
+Please give the first number-98
+Please give the second number-9
+The quotient is- 10
+The remainder is- 8
+[10, 8]
+Callable- True
+-------------3(B)-------------
+a is zero- False
+b is zero- False
+quotient is zero- False
+remainder is zero- False
+-------------3(C)-------------
+[10, 8, 4, 5, 6]
+The values greater than 4- [10, 8, 5, 6]
+-------------3(D)-------------
+{8, 10, 5, 6}
+-------------3(E)-------------
+The required immutable set frozenset({8, 10, 5, 6})
+-------------3(F)-------------
+The required max value is- 10
+The required hash value is- 10
+--------------4---------------
+Tony
+7
+Destructor called,The object is destroyed.
+-------------5(A)---------
+The updated salary of Mehak is- 70000
+-------------5(B)---------
+The record of Viren has been successfully deleted-
+-------------6-------------
+Please give a word-abc
+Give a word-cab
+Possible Words- ['abc', 'bac', 'bca', 'acb', 'cab', 'cba']
+If Barbie's word lies in the list,then their friendship is real.
+Friendship is real.
